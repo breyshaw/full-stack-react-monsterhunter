6d33a802bd309a1aa3f50db85f08b7f68a4a9906
breyshaw <dawsonbrey@gmail.com>
2021-11-27 12:14:51 -0500

    stub up controller functions

---
+++ controllers/monsters.js
@@ -0,0 +1,28 @@
+import { Monster } from "../models/monster.js";
+
+function index(req, res) {
+
+}
+
+function show(req,res) {
+
+}
+
+function create(req,res) {
+
+}
+
+function deleteMonster(req,res) {
+
+}
+
+function update(req,res) {
+
+}
+
+export {
+    index,
+    show,
+    deleteMonster as delete,
+    update
+}
